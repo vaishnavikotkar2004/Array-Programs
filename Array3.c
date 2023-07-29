137c7b93db5c8abd773e6336641017106fce8892
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-07-29 09:56:38 +0530

    Add files via upload

---
+++ Array3.c
@@ -0,0 +1,17 @@
+// Accept n numbers in an array and display sum of even numbers...
+#include<stdio.h>
+int main()
+{
+ int i,n,s=0,a[100];
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter n numbers");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ for(i=0;i<n;i++)
+ {
+  if(a[i]%2==0)
+  s=s+a[i];
+ }
+ printf("\n sum of even numbers=%d",s);
+}
---
+++ Array4.c
@@ -0,0 +1,16 @@
+// Accept n numbers in an array and display odd numbers...
+#include<stdio.h>
+int main()
+{
+ int i,n,a[100];
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter n numbers");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ for(i=0;i<n;i++)
+ {
+  if(a[i]%2!=0)
+  printf("\n odd numbers=%d",a[i]);
+ } 
+}
